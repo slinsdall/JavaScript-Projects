18e3da8c78e5b26090b1d1ffd7b565585920c0e7
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-01 16:46:06 -0700

    Adding files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var B = "\"Change will not come if we wait for some other person or some other time."
++ " We are the ones we\’ve been waiting for."
++ " We are the change that we seek.\""
++ "-Barack Obama" //This variable will display if using the document.write
+
+var Lunch = "Sandwich" , meat= "ham" , Sauce="Mayo" , Cheese="Cheddar", Vegetable="Lettuce" , bread="Sourdough";
+
+
+document.write(5 + 6); //This is using an expression
+
+window.alert("This is a string");//This is making an alert box when you open the webpage
+
+
+
