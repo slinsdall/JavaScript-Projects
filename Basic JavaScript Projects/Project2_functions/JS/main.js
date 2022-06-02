4f5594965420c91ec8a5b6a0f63886e6404da0f3
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-01 17:15:06 -0700

    Adding files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function Project2 () { //This is for the buttons displaying on the site
+    var str = "Click Here!";
+    var B = "Continue";
+    document.getElementById("Button").innerHTML = str;
+}
+
+function Project2() {
+    car sentence ="I am learning"; //This is for the p id
+    sentence += "a lot from this boot camp";
+    document.getElementById("Concatenate").innerHTML= sentence;
+}
