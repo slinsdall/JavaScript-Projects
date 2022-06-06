bb8e647c7863e2978ca17e404fee0669da8cabd6
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-06 13:24:14 -0700

    Update

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -34,10 +34,3 @@ function not_Function() {
     document.getElementById("Not").innerHTML = !(20>10);
 }
 
-function Ride_Function() {
-    var Height, Can_ride;
-    Height= document.getElementById("Height").ariaValueMax;
-    Can_ride = (Height <52) ? "You are too short" : "You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
-
-}
