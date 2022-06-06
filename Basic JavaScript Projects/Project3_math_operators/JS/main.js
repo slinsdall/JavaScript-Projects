2f9cbe9eede8e3d288822f9c5abd2d22b1c4ef94
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-06 11:41:44 -0700

    Updated

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,3 +1,4 @@
+
 function addition_function() {
     var addition = 10 + 6;
     document.getElementById("Math").innerHTML = "10 + 6 =" + addition;
@@ -27,3 +28,24 @@ function modulus_Operator() {
     var simple_math = 25 % 6;
     document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " +simple_math;
 }
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math").innerHTML = - x;
+}
+
+var X = 5;
+X++;
+document.write(X);
+
+var X = 5.25;
+X--;
+document.write(X);
+
+window.alert(Math.random());
+
+window.alert(Math.random()* 100);
+
+Math.PI;
+
+Math.round(4.6);
