c6d70b8dc3a8811f56a6aba13addc569d08a000b
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-06 13:15:11 -0700

    Update

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -37,7 +37,7 @@ function not_Function() {
 function Ride_Function() {
     var Height, Can_ride;
     Height= document.getElementById("Height").ariaValueMax;
-    Can_ride = (Height <52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + "to ride.";
-    
+    Can_ride = (Height <52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+
 }
