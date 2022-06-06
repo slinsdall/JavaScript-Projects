28d47a9c9584f6c4118c1f8c32c02cbf2f7faed9
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-06 11:44:18 -0700

    Update to main.js

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,30 +1,30 @@
 
-function addition_function() {
+function addition_function() { //This is to show addition using this function
     var addition = 10 + 6;
     document.getElementById("Math").innerHTML = "10 + 6 =" + addition;
 }
 
-function subtraction_Function() {
+function subtraction_Function() {//This function is for subtraction
     var Subtraction = 5 - 2;
     document.getElementById("Math").innerHTML = "5 - 2 =" + Subtraction;
 }
 
-function multiplication() {
+function multiplication() {//This function is for multiplying
     var simple_Math = 6 * 8;
     document.getElementById("Math").innerHTML = "6*8=" + simple_Math;
 }
 
-function division() {
+function division() {//This function is to divide
     var simple = 48/6;
     document.getElementById("Math").innerHTML = "48/6=" + simple;
 }
 
-function more_math() {
+function more_math() { //This function is to do multiple operations within one function
     var mulitple_math=(1+2) * 10 /2-5;
     document.getElementById("Math").innerHTML = "1 plus 1, multiplied by 10, divided in half and then subtracted by 5 equals" + mulitple_math;
 }
 
-function modulus_Operator() {
+function modulus_Operator() {//This function is to get the remainder using the % symbol
     var simple_math = 25 % 6;
     document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " +simple_math;
 }
