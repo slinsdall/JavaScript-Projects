503f11efbe1e4d0f95050d2411cae7caf76ba5ea
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-06 11:27:22 -0700

    Updating

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,10 @@
 function Project2 () { //This is for the buttons displaying on the site
     var str = "Click Here!";
-    var B = "Continue";
     document.getElementById("Button").innerHTML = str;
 }
 
 function Button_click() {
-    car sentence ="I am learning"; //This is for the p id
+    var sentence ="I am learning "; //This is for the p id
     sentence += "a lot from this boot camp";
     document.getElementById("Concatenate").innerHTML= sentence;
 }
---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,29 @@
+function addition_function() {
+    var addition = 10 + 6;
+    document.getElementById("Math").innerHTML = "10 + 6 =" + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 =" + Subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6*8=" + simple_Math;
+}
+
+function division() {
+    var simple = 48/6;
+    document.getElementById("Math").innerHTML = "48/6=" + simple;
+}
+
+function more_math() {
+    var mulitple_math=(1+2) * 10 /2-5;
+    document.getElementById("Math").innerHTML = "1 plus 1, multiplied by 10, divided in half and then subtracted by 5 equals" + mulitple_math;
+}
+
+function modulus_Operator() {
+    var simple_math = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " +simple_math;
+}
