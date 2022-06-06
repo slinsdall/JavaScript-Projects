74b5d8763e549f08a55cfa8bb1eaddeb0a98ed4d
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-06 13:56:10 -0700

    Updates

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,7 +1,15 @@
 function Ride_Function() {
     var Height, Can_ride;
-    Height= document.getElementById("Height");
-    Can_ride = (Height <52) ? "You are too short" : "You are tall enough";
+    Height =  document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+
+}
+
+function Vote_Function() {
+    var Age, Can_vote;
+    Age =  document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough to " : "You can";
+    document.getElementById("Vote").innerHTML = Can_vote + " vote.";
 
 }
