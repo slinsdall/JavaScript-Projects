ee12755096a53ff6817613e0fc099f469b9a4ed7
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-08 14:38:31 -0700

    Update

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,40 @@
+function full_sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+function slice_Method() {
+    var sentence ="All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+let text = "Hello World!";
+let result = text.toUpperCase();
+document.getElementByID("demo").innerHTML = result;
+
+let text = "This is Blue";
+let position = text.search("Blue");
+document.getElementById("search").innerHTML = position;
+
+function string_Method() {
+    var X = 182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+
+let num = 5.56789;
+let n = num.toFixed();
+document.getElementById("fixed").innerHTML = n;
+
+let text = "This is value method.";
+let result= text.valueOf();
+document.getElementById("value").innerHTML = result;
