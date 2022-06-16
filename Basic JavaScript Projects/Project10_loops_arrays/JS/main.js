c875cb86506b02f090375272cf99ed2ed27ca923
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-06-16 14:57:32 -0700

    Updating

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,67 @@
+function Call_Loop() { //This is a while statement
+    var Digit ="";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+let string = "Hello World!";//This shows the string length property
+let length = string.length;
+document.getElementById("demo").innerHTML = length;
+
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() { // This shows the for loop
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_Function() {
+    var Cat_Picture = [];
+    Cat_Picure[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picure[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Array").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
+
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"fender", color:"black"};
+    Musical_Instrument.color="black";
+    Musical_Instrument.price ="$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+
+document.getElementById("name").innerHTML = myFunction("Sarah");
+
+function myFunction(name) {
+    return "Hello " + name;//This shows the return method
+    
+}
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+let text = ""; //This is for a break in a function
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { break; }
+  text += "The number is " + i + "<br>";
+}
+
+document.getElementById("test").innerHTML = text;
